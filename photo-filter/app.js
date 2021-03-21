bff26089d0c93cbc24ac96b60cd4a0f0fce37153
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-03-21 18:57:34 +0300

    feat: next picture button

--- photo-filter/app.js
+++ photo-filter/app.js
@@ -6,9 +6,21 @@ const defaultFilters = {
     "--hue": [0,"deg"]
 }
 
+const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg','11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+
+
+let dayTime = ["morning/","day/","evening/","night/"]
+let imgIndex = 0;
+
 const filters = document.querySelector('.filters');
 const resetButton = document.querySelector(".btn-reset")
 const fullScreenButton = document.querySelector(".fullscreen")
+const image = document.querySelector("img[alt=image]");
+const nextPicture = document.querySelector(".btn-next")
+
+
 
 let isMouseDown = false;
 
@@ -57,6 +69,45 @@ function toggleFullScreen() {
     }
   }
 
+function getImg()
+{
+    let source = base; 
+
+    const now = new Date();
+
+    if(now.getHours >= 6 && now.getHours <= 11)
+    {
+        source += dayTime[0];
+    }
+    else if(now.getHours >= 12 && now.getHours <= 17)
+    {
+        source += dayTime[1];
+    }
+    else if(now.getHours >= 18 && now.getHours <= 23){
+        source += dayTime[2];
+    }
+    else{
+        source += dayTime[3];
+    }
+
+    const index = imgIndex % images.length;
+    source += images[index];
+    viewBgImage(source)
+    imgIndex += 1;
+    nextPicture.disabled = true;
+    setTimeout(() => nextPicture.disabled = false, 1000)
+}
+
+function viewBgImage(src) 
+{  
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {      
+       image.src = src;
+    }; 
+}
+
+
 window.addEventListener('mousedown', e => isMouseDown = true)
 window.addEventListener("mouseup", e => isMouseDown = false)
 
@@ -64,4 +115,8 @@ filters.addEventListener("mousemove", e=> inputHandler(e))
 
 resetButton.addEventListener("click", e => resetFilters())
 
-fullScreenButton.addEventListener("click", e=> toggleFullScreen())
+fullScreenButton.addEventListener("click", e=> toggleFullScreen());
+
+nextPicture.addEventListener("click",e=> getImg())
+
+console.log(1 % images.length)
